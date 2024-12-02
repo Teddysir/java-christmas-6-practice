36bbe78bec93fddb4e9edf5fc4ccae167ecf1e96
Teddysir <sdzx0719@naver.com>
2024-12-02 13:59:31 +0900

    feat: 올바른 날짜 형식이 아닐 경우 예외 처리 구현

--- src/main/java/christmas/constants/ErrorMessageType.java
+++ src/main/java/christmas/constants/ErrorMessageType.java
@@ -2,7 +2,8 @@ package christmas.constants;
 
 public enum ErrorMessageType {
 
-    INVALID_ORDER_AMOUNT("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력 해주세요.");
+    INVALID_ORDER_AMOUNT("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력 해주세요."),
+    INVALID_DATE_FORMAT("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/christmas/utils/ServiceValidation.java
+++ src/main/java/christmas/utils/ServiceValidation.java
@@ -2,11 +2,21 @@ package christmas.utils;
 
 import christmas.constants.ErrorMessageType;
 
+import java.util.IllegalFormatException;
+
 public class ServiceValidation {
 
     public static void checkedTotalOrderAmount(int totalOrderAmount) {
         if(totalOrderAmount > 20 ) {
             throw new IllegalArgumentException(ErrorMessageType.INVALID_ORDER_AMOUNT.getMessage());
+        }
+    }
+
+    public static void checkedDateFormat(String rawDate) {
+        try {
+             Integer.parseInt(rawDate);
+        } catch (IllegalFormatException e) {
+            throw new IllegalArgumentException(ErrorMessageType.INVALID_DATE_FORMAT.getMessage());
         }
     }
 
