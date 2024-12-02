1a656e60fd3b80b85e20093bfb9ad37bb76b64e4
Teddysir <sdzx0719@naver.com>
2024-12-02 14:39:34 +0900

    feat: 메뉴 콤마와 하이픈으로 분리하는 메서드 구현

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,8 +1,14 @@
 package christmas.controller;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.model.Restaurant;
 import christmas.utils.ServiceValidation;
+import christmas.utils.Splitter;
 import christmas.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Controller {
 
@@ -10,13 +16,31 @@ public class Controller {
 
     public void start() {
         restaurant.initializeMenu();
-        inputVisitDate();
+        int visitDate = getVisitDate();
+        getOrderMenus();
     }
 
 
-    public void inputVisitDate() {
+    private int getVisitDate() {
         String rawDate = InputView.inputVisitDateMessage();
-        int visitDate = ServiceValidation.checkedDateFormat(rawDate);
-
+        return ServiceValidation.checkedDateFormat(rawDate);
     }
+
+    private void getOrderMenus() {
+        InputView.inputOrderMenuMessage();
+        String rawMenu = Console.readLine().trim();
+
+        List<String> splitCommaMenus = Splitter.commaSplitter(rawMenu);
+
+        calculateMenu(splitCommaMenus);
+    }
+
+    private void calculateMenu(List<String> splitCommaMenus) {
+        for (String splitCommaMenu : splitCommaMenus) {
+            String[] splitHyphenMenu = Splitter.hyphenSplitter(splitCommaMenu);
+            System.out.println(Arrays.toString(splitHyphenMenu));
+        }
+    }
+
+
 }
--- src/main/java/christmas/utils/Splitter.java
+++ src/main/java/christmas/utils/Splitter.java
@@ -1,9 +1,17 @@
 package christmas.utils;
 
+import christmas.model.Menu;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Splitter {
 
-    public static String[] commaSplitter(String rawOrderList) { // 제로콜라-3, 바베큐립,2 -> [제로콜라-3], [바베큐립-2]
-        return rawOrderList.split(",");
+    public static List<String> commaSplitter(String rawOrderList) { // 제로콜라-3, 바베큐립,2 -> [제로콜라-3], [바베큐립-2]
+        return Arrays.stream(rawOrderList.split(","))
+                .map(String::trim)
+                .toList();
     }
 
     public static String[] hyphenSplitter(String rawHyphenMenu) { // 제로콜라-3 -> [제로콜라], [3]
